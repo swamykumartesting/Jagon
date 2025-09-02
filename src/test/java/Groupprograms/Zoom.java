450a17de81f0a836776b707e181fef6ef438e8b2
Charvik <Charvik@LAPTOP-S0IS0QVR>
2025-09-02 09:05:09 +0530

    my first works

---
+++ src/test/java/Groupprograms/Zoom.java
@@ -0,0 +1,19 @@
+package Groupprograms;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Zoom {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
